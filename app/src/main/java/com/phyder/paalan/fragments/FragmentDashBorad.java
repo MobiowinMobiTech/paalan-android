f89d3a2e0b739f955d792bd4cbf083dd9a30bcb4
Yashica Taneja <yashica.taneja@cmss.in>
2017-01-17 19:12:32 +0530

    Event Creation

--- app/src/main/java/com/phyder/paalan/fragments/FragmentDashBorad.java
+++ app/src/main/java/com/phyder/paalan/fragments/FragmentDashBorad.java
@@ -1,5 +1,6 @@
 package com.phyder.paalan.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
@@ -7,12 +8,9 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.phyder.paalan.R;
-import com.phyder.paalan.adapter.CustomListAdapter;
 import com.phyder.paalan.adapter.SlidingImageAdapter;
 
 import java.util.ArrayList;
@@ -31,44 +29,37 @@ public class FragmentDashBorad extends android.support.v4.app.Fragment {
     private ViewPager mPager;
     private static int currentPage = 0;
     private static int NUM_PAGES = 0;
+  //  CustomListAdapter customListAdapter;
     private static final Integer[] IMAGES = {R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d};
     private ArrayList<Integer> ImagesArray = new ArrayList<Integer>();
 
-    ListView list;
-    String[] itemname ={"Publish event", "Achievements", "Event history", "Social Strength", "Social Request", "About us", "Contact us"};
+    String[] itemname = {"Publish event", "Achievements", "Event history", "Social Strength", "Social Request", "About us", "Contact us"};
 
-    Integer[] imgid={R.drawable.ic_publish_event, R.drawable.ic_achievments, R.drawable.event_history, R.drawable.social_strength,
+    int[] imgid = {R.drawable.ic_publish_event, R.drawable.ic_achievments, R.drawable.event_history, R.drawable.social_strength,
             R.drawable.ic_social_request, R.drawable.ic_about_us, R.drawable.contact_us};
 
-//    GridView gridView;
-//  ArrayList<Item> gridArray = new ArrayList<Item>();
-//    CustomGridViewAdapter customGridViewAdapter;
-    SlidingImageAdapter slidingImageAdapter;
-  ArrayList<Integer> dashboardImages = new ArrayList<>();
+
+    SlidingImageAdapter SlidingImageAdapter;
+    ArrayList<Integer> dashboardImages = new ArrayList<>();
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
         View view = inflater.inflate(R.layout.dashboard_framelayout, container, false);
         mPager = (ViewPager) view.findViewById(R.id.image_pager);
-           init();
 
-        CustomListAdapter adapter = new CustomListAdapter(getActivity(),0, itemname, imgid);
-        list = (ListView) view.findViewById(R.id.list);
-        list.setAdapter(adapter);
+//        ListView list;
+//        Context context;
+//
+//        list = (ListView) view.findViewById(R.id.list);
+//        list.setAdapter(new CustomListAdapter(getActivity(), itemname, imgid));
 
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String Slecteditem = itemname[+position];
-                Toast.makeText(getActivity(), Slecteditem, Toast.LENGTH_SHORT).show();
-
-            }
-        });
+        init();
         return view;
     }
-//
+
+
+
 
     private void init() {
         {
@@ -97,36 +88,15 @@ public class FragmentDashBorad extends android.support.v4.app.Fragment {
                 public void run() {
                     handler.post(Update);
                 }
-            }, 3000, 3000);
+            }, 1500, 1500);
         }
     }
-
 
 }
 
 
 
-//        gridArray.add(new Item(R.drawable.ic_publish_event, "Publish event"));
-//        gridArray.add(new Item(R.drawable.ic_achievments, "Achievements"));
-//        gridArray.add(new Item(R.drawable.event_history, " Event history"));
-//        gridArray.add(new Item(R.drawable.social_strength, " Social Strength"));
-//        gridArray.add(new Item(R.drawable.ic_social_request, "  Social Request"));
-//        gridArray.add(new Item(R.drawable.ic_about_us, "About us"));
-//        gridArray.add(new Item(R.drawable.contact_us, "Contact us"));
-//
-//
-//        gridView = (GridView) view.findViewById(R.id.gridView1);
-//        customGridViewAdapter = new CustomGridViewAdapter(getActivity(), R.layout.row_grid, gridArray);
-//        gridView.setAdapter(customGridViewAdapter);
-//
-//        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Toast.makeText(getActivity(),"Selected "+gridArray.get(position).getTitle(),Toast.LENGTH_LONG).show();
-//            }
-//        });
-//        return view;
 
-//    }
+
 
 
